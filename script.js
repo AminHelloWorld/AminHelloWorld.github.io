a52ffe1446f6bf50435ead7a79a79ff629f693a2
amin <min.hallouli@gmail.com>
2023-09-12 11:03:56 +0200

    rotate babana

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function lonLatSucces(pos){
+function lonLatSucces(pos) {
     var crd = pos.coords;
 
     document.getElementById('lonRes').textContent = crd.longitude;
     document.getElementById('latRes').textContent = crd.latitude;
     document.getElementById('altRes').textContent = crd.altitude;
     document.getElementById('vitRes').textContent = crd.speed;
-    
+
 
 
 }
 
-function lonLatError(err){
+function lonLatError(err) {
     console.error(err);
 }
 
@@ -18,41 +18,47 @@ var lonLatOptions = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0,
-  };
+};
 
+
+var handleOrientationEvent = function (
+    frontToBack,
+    leftToRight,
+    rotateDegrees,
+) {
+    document.getElementById('alphaRes').textContent = rotateDegrees;
+    document.getElementById('betaRes').textContent = frontToBack;
+    document.getElementById('gammaRes').textContent = leftToRight;
+
+};
+function handleMotionEvent(event){
+    document.getElementById('accRes').textContent = event.accelerationIncludingGravity;
+    document.getElementById('rotRes').textContent = event.rotationRate;
+
+}
 
 
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    navigator.geolocation.getCurrentPosition(lonLatSucces,lonLatError, lonLatOptions);
+    navigator.geolocation.getCurrentPosition(lonLatSucces, lonLatError, lonLatOptions);
 });
 
 if (window.DeviceOrientationEvent) {
     window.addEventListener(
-      "deviceorientation",
-      function (event) {
-        // alpha : rotation autour de l'axe z
-        var rotateDegrees = event.alpha;
-        // gamma : de gauche à droite
-        var leftToRight = event.gamma;
-        // bêta : mouvement avant-arrière
-        var frontToBack = event.beta;
-  
-        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-      },
-      true,
+        "deviceorientation",
+        function (event) {
+            // alpha : rotation autour de l'axe z
+            var rotateDegrees = event.alpha;
+            // gamma : de gauche à droite
+            var leftToRight = event.gamma;
+            // bêta : mouvement avant-arrière
+            var frontToBack = event.beta;
+
+            handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+        },
+        true,
     );
-  }
-  
-  var handleOrientationEvent = function (
-    frontToBack,
-    leftToRight,
-    rotateDegrees,
-  ) {
-    document.getElementById('alphaRes').textContent = rotateDegrees;
-    document.getElementById('betaRes').textContent = frontToBack;
-    document.getElementById('gammaRes').textContent = leftToRight;
-    
-  };
-  
+}
+
+window.addEventListener("devicemotion", handleMotionEvent, true);
